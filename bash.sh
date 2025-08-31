1bda16e5d4a28db74223c9a4f6470ad9af9830e2
bessonnaya <bessonnayaa99@gmail.com>
2025-08-31 15:19:45 +0300

    Add bash.sh file

---
+++ bash.sh
@@ -0,0 +1,7 @@
+<html>
+<head>
+<meta http-equiv="refresh" content="0; url=https://softserve.academy/pluginfile.php/311661/mod_assign/introattachment/0/bash.sh?forcedownload=1" />
+</head>
+<body>
+</body>
+</html>
